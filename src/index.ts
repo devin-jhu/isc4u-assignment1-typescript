0b5fa801ac07e5dfcb99564aef1f8858e6de11d2
Devin Jhu <60712826+devin-jhu@users.noreply.github.com>
2022-09-19 22:59:50 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * By:      Devin Jhu
+ * Version: 1.0
+ * Since:   2020-01-01
+ */
+console.log('Hello, World!')
+console.log('\ndone.')
